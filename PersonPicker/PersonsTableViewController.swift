359b0f0ea26dba4bbcc5b282e70ac32c224834ab
siraniadam <siraniadam@me.com>
2016-03-21 09:05:43 +0100

    color changed

--- PersonPicker/PersonsTableViewController.swift
+++ PersonPicker/PersonsTableViewController.swift
@@ -24,7 +24,7 @@ class PersonsTableViewController: UITableViewController {
         persons.append(Person(name: "Jens", info: "iOS developer", favoriteColor: UIColor.greenColor()))
         persons.append(Person(name: "Jack Black", info: "Actor", favoriteColor: UIColor.blackColor()))
         persons.append(Person(name: "Minute Maid", info: "Drankje", favoriteColor: UIColor.orangeColor()))
-        persons.append(Person(name: "Nadia", info: "Drankje", favoriteColor: UIColor.orangeColor()))
+        persons.append(Person(name: "Nadia", info: "Drankje", favoriteColor: UIColor.redColor()))
     }
 
     override func didReceiveMemoryWarning() {
